6ce7f9973c959271adac79dab606a1a721a2c096
Andy Aylward <aaylward@gmail.com>
2018-01-18 21:34:18 -0500

    add setup thing

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt update && sudo apt -y upgrade
+sudo apt -y install curl git build-essential python-dev python3-dev ninja-build vim vim-gtk3 zsh htop tree
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:aaylward/dotfiles.git ~/git/dotfiles
+git clone git@github.com:tpope/vim-vividchalk.git ~/git/vividchalk
+
+chsh -s /usr/bin/zsh
+mkdir -p ~/.vim/colors
+cp ~/git/vividchalk/colors/vividchalk.vim ~/.vim/colors
+cp ~/git/dotfiles/.zshrc ~
+
+echo "source $HOME/git/dotfiles/vimrc" > ~/.vimrc
+

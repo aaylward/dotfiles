861b7b4c20718cc896b70acfcf8bee6c9aed8649
Andy Aylward <aaylward@gmail.com>
2018-04-29 09:07:20 -0400

    add fedora setup

---
+++ setup-fedora.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install gcc ghc curl git make automake cmake gvim perf powertop python-devel zsh gnome-tweak-tool clang xcape
+
+# make video in firefox work
+sudo dnf install ffmpeg-libs compat-ffmpeg28
+
+# make YouCompleteMe work without --system-clang
+sudo dnf install ncurses-compat-libs
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+nvm install --lts
+
+# make building and running Signal-Desktop possible
+sudo dnf install libXScrnSaver
+npm i -g yarn
+
+# How to solve SELinux problems with expressvpn?
+# sudo semanage fcontext -a -t net_conf_t /var/lib/expressvpn/resolv.conf
+# sudo restorecon -Rv /var/lib/expressvpn
+
+git config --global user.name "Andy Aylward"
+git config --global user.email "aaylward@gmail.com"
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    ssh-keygen
+    echo "add key to github: https://github.com/settings/ssh/new"
+    cat ~/.ssh/id_rsa.pub
+    read -n 1 -s -r -p "Press any key to continue once your new key is uploaded"
+fi
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:aaylward/dotfiles.git ~/src/dotfiles
+git clone git@github.com:tpope/vim-vividchalk.git ~/src/vividchalk
+
+chsh -s /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir -p ~/.vim/colors
+cp ~/src/vividchalk/colors/vividchalk.vim ~/.vim/colors
+cp ~/src/dotfiles/.zshrc ~
+
+source ~/.zshrc
+
+curl https://sh.rustup.rs -sSf | sh
+
+if [ ! -f ~/.vimrc ]; then
+    echo "source $HOME/src/dotfiles/vimrc" > ~/.vimrc
+    vim +PluginInstall +qall
+fi
+
